21568de327a3c1c3fe8aead633997017bea3c6ae
lihao779 <3024978915@qq.com>
2020-04-25 23:11:30 +0800

    .

---
+++ Test-3-25/Test-3-25/tets.c
@@ -0,0 +1,16 @@
+﻿//字符串逆序
+#include<stdio.h>
+int main()
+{
+	int n, m;
+	scanf("%d %d", &n, &m);
+	int temp = 0;
+	int sum = 0;
+	for (int i = 0; i < m; i++)
+	{
+		temp = temp * 10 + n;
+		sum += temp;
+
+	}
+	printf("%d\n", sum);
+}
